9c5fbb7bd673609d93ed416ec5e6c33566720b0c
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2024-09-12 15:54:02 +0600

    add linked in logo

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,7 @@
-import { MapPinHouseIcon, MapPinIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaLinkedin } from "react-icons/fa";
+import { LuMapPin } from "react-icons/lu";
 const Hero = () => {
   return (
     <section className="min-h-screen mx-4 flex flex-col justify-center md:max-w-screen-md md:mx-auto">
@@ -15,7 +16,7 @@ const Hero = () => {
       <div className="space-y-1 mt-12">
         <h3 className="text-2xl text-yellow-50">Web & App Developer</h3>
         <div className="flex gap-1 items-center text-yellow-100">
-          <MapPinIcon size={18} />
+          <LuMapPin size={18} />
           <p className="text-md text-center sm:text-left">Dhaka, Bangladesh</p>
         </div>
       </div>
@@ -32,16 +33,39 @@ const Hero = () => {
           </p>
         </div>
         <div className="hidden md:block md:col-span-5 space-y-2">
-          <h2 className="text-2xl font-bold">Contact</h2>
+          <div className="flex items-baseline justify-between">
+            <h2 className="text-2xl font-bold">Contact</h2>
+            <Link
+              href="https://www.linkedin.com/in/sheikh-sulaiman/"
+              className="hover:text-[#0077B5]"
+            >
+              <FaLinkedin size={24} />
+            </Link>
+          </div>
           <ul>
-            <li className="hover:underline hover:text-brand">
-              <a href="mailto:sheikhsulaimansony@outlook.com">
+            <li>
+              <Link
+                className="hover:underline hover:text-brand"
+                href="mailto:sheikhsulaimansony@outlook.com"
+              >
                 sheikhsulaimansony@outlook.com
-              </a>
+              </Link>
             </li>
-            <li className="hover:text-brand">+880 19471 97174</li>
-            <li className="hover:underline hover:text-brand">
-              <Link href="https://sonyed.tech">https://sonyed.tech</Link>
+            <li>
+              <Link
+                className="hover:text-brand"
+                href={"https://wa.me/+8801947197174"}
+              >
+                +880 19471 97174
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:underline hover:text-brand"
+                href="https://sonyed.tech"
+              >
+                https://sonyed.tech
+              </Link>
             </li>
           </ul>
         </div>
