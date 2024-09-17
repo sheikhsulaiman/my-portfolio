44f638fdc1474d2bff291d24101b9db468a9f3b5
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2024-09-17 18:50:34 +0600

    can i talk to you

--- app/start-a-project/page.tsx
+++ app/start-a-project/page.tsx
@@ -1,4 +1,6 @@
 import NewProjectForm from "@/components/forms/new-project-form";
+import Link from "next/link";
+import { LuMail } from "react-icons/lu";
 import React from "react";
 
 const StartAProjectPage = () => {
@@ -9,6 +11,30 @@ const StartAProjectPage = () => {
           Tell me about <br /> your <span className="text-brand">project</span>
         </h1>
         <NewProjectForm />
+        <div className="flex flex-col gap-2 my-8">
+          <h3 className="font-bold text-4xl">
+            Can I talk to you <span className="text-brand">directly?</span> Yes
+            you can!
+          </h3>
+          <Link
+            href="mailto:sheikhsulaimansony@outlook.com"
+            className="flex items-center justify-start gap-2"
+          >
+            <LuMail size={24} />
+            <p className="text-2xl hover:text-brand hover:underline">
+              sheikhsulaimansony@outlook.com
+            </p>
+          </Link>
+          <Link
+            href="mailto:sonyedtech@gmail.com"
+            className="flex items-center justify-start gap-2"
+          >
+            <LuMail size={24} />
+            <p className="text-2xl hover:text-brand hover:underline">
+              sonyedtech@gmail.com
+            </p>
+          </Link>
+        </div>
       </div>
     </main>
   );
