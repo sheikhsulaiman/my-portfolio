cc63a39f2cc898aaa862904e491d74913b426718
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2024-09-17 19:03:55 +0600

    add promise to nodemailer

--- app/api/project-proposal/route.ts
+++ app/api/project-proposal/route.ts
@@ -26,6 +26,19 @@ export async function POST(req: NextRequest) {
     needToknow: body.needToKnow,
   });
 
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        // console.log(error);
+        reject(error);
+      } else {
+        // console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
   const mailOptions = {
     from: "sonyedtech@gmail.com",
     to: "sheikhsulaimansony@outlook.com",
@@ -34,19 +47,22 @@ export async function POST(req: NextRequest) {
   };
 
   try {
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (!error) {
-        return NextResponse.json(
-          {
-            message:
-              "Thank you for your cooperation. I will contact you shortly.",
-            info: info,
-          },
-          { status: 200 }
-        );
-      } else {
-        return NextResponse.json({ error: error }, { status: 500 });
-      }
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (!error) {
+          resolve(info);
+          return NextResponse.json(
+            {
+              message:
+                "Thank you for your cooperation. I will contact you shortly.",
+            },
+            { status: 200 }
+          );
+        } else {
+          reject(error);
+          return NextResponse.json({ error: error }, { status: 500 });
+        }
+      });
     });
 
     return NextResponse.json(
