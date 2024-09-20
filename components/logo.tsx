c97f069a7ccbb86fc8ed845f50b0c60e8affd73e
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2024-09-20 06:36:37 +0600

    logo according to theme mode

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,17 +1,28 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 const Logo = () => {
-  return (
+  const { theme } = useTheme();
+
+  return theme === "dark" ? (
     <Link href="/">
-      <div className="bg-primary-foreground rounded-md">
-        <Image
-          src={"/images/logo.png"}
-          height={75}
-          width={200}
-          alt="sonyed.tech"
-        />
-      </div>
+      <Image
+        src={"/images/logo_dark.png"}
+        height={75}
+        width={200}
+        alt="sonyed.tech"
+      />
+    </Link>
+  ) : (
+    <Link href="/">
+      <Image
+        src={"/images/logo_light.png"}
+        height={75}
+        width={200}
+        alt="sonyed.tech"
+      />
     </Link>
   );
 };
